7b514c019822544d5bd240cf641da3e3be71a2ee
James Hattenhauer <jhattenhauer@outlook.com>
2025-11-14 09:48:26 -0330

    broke tab into beats

--- libraries/tab_translater.py
+++ libraries/tab_translater.py
@@ -3,19 +3,23 @@ import logging
 import os
 
 
-tab_file = "/home/ubuntu/projects/SoloPlayer/libraries/tabs/creed-one_last_breath.gp3"
+tab_file = "tabs/creed-one_last_breath.gp3"
 song = guitarpro.parse(tab_file)
 
-log_file = "/home/ubuntu/projects/SoloPlayer/libraries/track_output.log"
+log_file = "track_output.log"
 if os.path.exists(log_file):
     os.remove(log_file)
 
-logging.basicConfig(filename='/home/ubuntu/projects/SoloPlayer/libraries/track_output.log', level=logging.DEBUG)
+logging.basicConfig(filename='track_output.log', level=logging.DEBUG)
 
+lead_guitar = song.tracks[0]
+
+logging.debug(f"{song.title}")
 for track in song.tracks:
     logging.debug(f"{track.name}")
     for measure in track.measures:
         for voice in measure.voices:
             for beat in voice.beats:
+                logging.debug(f"\nbreak note\n")
                 for note in beat.notes:
-                    logging.debug(f"String: {note.string}, Fret: {note.value}")  # value = fret
+                    logging.debug(f"String: {note.string}, Fret: {note.value}")            
