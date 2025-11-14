b06a985ff26b0c37f4027efa6ed0c1ea37db54fe
James Hattenhauer <jhattenhauer@outlook.com>
2025-11-14 09:01:48 -0330

    working on tab parsing

--- libraries/tab_translater.py
+++ libraries/tab_translater.py
@@ -1,4 +1,15 @@
 import guitarpro
+import logging
 
-file = input("Input path to guitar tab")
-tab = guitarpro.parse(file)
+file_path = "/home/ubuntu/projects/SoloPlayer/libraries/creed-one_last_breath.gp3"
+song = guitarpro.parse(file_path)
+
+logging.basicConfig(filename='track_output.log', level=logging.DEBUG)
+
+for track in song.tracks:
+    logging.debug(f"{track.name}")
+    for measure in track.measures:
+        for voice in measure.voices:
+            for beat in voice.beats:
+                for note in beat.notes:
+                    logging.debug(f"String: {note.string}, Fret: {note.value}")  # value = fret
